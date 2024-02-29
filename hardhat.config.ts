8e98a30d85cd0bd5b13c2c683da2d59713a50dc4
nlok5923 <nlokhande5923@gmail.com>
2024-02-29 16:21:57 +0530

    add base contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
+import dotenv from 'dotenv';
 import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    polygonzkevm: {
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/cNkdRWeB8oylSQJSA2V3Xev2PYh5YGr4',
+      accounts:[`${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
 
 export default config;
