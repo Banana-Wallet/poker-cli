e3178eed695293c34f23eb19c2913acfbb5fe400
nlok5923 <nlokhande5923@gmail.com>
2024-03-06 15:21:54 +0400

    fixes

--- game/cli.ts
+++ game/cli.ts
@@ -7,7 +7,8 @@ const pvKey1 = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff
 const pvKey2 = '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d';
 const pvKey3 = '0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a';
 const pvKey4 = '0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6';
-const jsonRpcProvider = new ethers.JsonRpcProvider('http://127.0.0.1:8545');
+const localNodeRpc = 'http://127.0.0.1:8545';
+const jsonRpcProvider = new ethers.JsonRpcProvider(localNodeRpc);
 
 const player1 = new ethers.Wallet(pvKey1, jsonRpcProvider);
 const player2 = new ethers.Wallet(pvKey2, jsonRpcProvider);
